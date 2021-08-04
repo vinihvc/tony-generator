7aae04a6363ee7451e22e28aa565be5cd389a26a
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2021-08-03 22:32:21 -0300

    chore(config): add dark mode by class on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  darkMode: 'class',
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   // darkMode: 'media',
