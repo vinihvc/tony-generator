94838609ad38f53a9c09b040bc2436df02c38726
Vinicius Vicentini <viniciusvicentini@live.com>
2021-08-26 20:15:29 -0300

    chore: improve Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,31 @@
 import React, { HTMLAttributes } from 'react'
 
+import clsx from 'clsx'
+
 type ButtonProps = {
+  size?: 'sm' | 'md' | 'lg'
   children?: React.ReactNode
 } & HTMLAttributes<HTMLButtonElement>
 
-const Button = ({ children, ...props }: ButtonProps) => {
+const baseStyle =
+  'flex justify-center items-center font-medium rounded-full transition duration-300 ease-in cursor-pointer focus:outline-none text-white bg-twitter-500 hover:bg-twitter-600'
+
+const sizes = {
+  sm: 'px-5 h-8',
+  md: 'px-5 h-10 text-sm',
+  lg: 'px-10 h-12'
+}
+
+const Button = ({
+  size = 'md',
+  className,
+  children,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       type="button"
-      className="flex justify-center p-4 mx-auto w-full lg:w-3/5 font-bold tracking-wide text-gray-100 bg-twitter-500 hover:bg-twitter-600 rounded-full shadow-lg transition duration-300 ease-in cursor-pointer focus:outline-none"
+      className={clsx(baseStyle, sizes[size], className)}
       {...props}
     >
       {children}
