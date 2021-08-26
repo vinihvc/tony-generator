524c0f7718ed65934d54e768fb22652bcaa182c3
Vinicius Vicentini <viniciusvicentini@live.com>
2021-08-26 20:16:00 -0300

    chore: remove relative style from Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,8 +8,8 @@ const Input = ({ value, ...props }: InputProps) => {
   return (
     <input
       type="text"
-      className="relative py-2 px-4 w-full text-sm sm:text-base placeholder-gray-400 text-black rounded-full border focus:border-twitter-500 focus:outline-none"
       value={value}
+      className="py-2 px-4 w-full text-sm placeholder-gray-400 text-black rounded-full border focus:border-twitter-500 focus:outline-none"
       {...props}
     />
   )
