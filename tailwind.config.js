adb9add603faf4047fe67a08a438c9b16b97d146
Vinicius Vicentini <viniciusvicentini@live.com>
2021-08-26 20:15:10 -0300

    chore: renaming color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   darkMode: 'class',
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  // darkMode: 'media',
   theme: {
     extend: {
       fontWeight: ['hover', 'focus'],
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
     },
-    colors:{
+    colors: {
       ...defaultTheme.colors,
-      twitter:{
-        500:'#1da1f2',
-        600:'#1A91DA'
+      twitter: {
+        500: '#1da1f2',
+        600: '#1A91DA'
       },
-      fav: '#E0245E'
+      like: '#E0245E'
     }
   },
   variants: {},
   plugins: []
-};
+}
