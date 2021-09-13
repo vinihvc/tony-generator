3e5d4b2ad18fd4d72d76f83ac98f57f76325310b
Vinicius Vicentini <viniciusvicentini@live.com>
2021-09-13 14:13:58 -0300

    chore: add comments

--- src/utils/tag-someone.ts
+++ src/utils/tag-someone.ts
@@ -1,12 +1,22 @@
+// regex to match @someone
 const findTagRegex = /(\S*@\[[^\]]+\])|(\S*@\S+)/gi
-
-const addTagClassName = (text: string) => {
-  return `<span class='text-twitter-500'>${text}</span>`
-}
 
 const defaultText =
   'Quem faz código bom é quem não se garante com bug em produção'
 
+/**
+ * Add span with blue text
+ */
+const addTagClassName = (text: string) => {
+  return `<span class='text-twitter-500'>${text}</span>`
+}
+
+/**
+ * Match @someone in text
+ * ```md
+ * “Quero uma talk do @viniciushvc” - Tony 2021
+ * ```
+ */
 const tagSomeone = (text: string) => {
   const newText = text.replace(findTagRegex, addTagClassName)
 
