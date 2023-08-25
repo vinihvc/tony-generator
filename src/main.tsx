310e807a0d90d97c44db6468f669189ccbe5e2dd
Vinicius Vicentini <viniciusvicentini@live.com>
2023-08-25 17:56:39 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import './styles/global.css'
 
-import App from './App'
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
